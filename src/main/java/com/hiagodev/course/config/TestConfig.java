a2c9c0dffa15ae8bb96628cb091e5fe9546c5890
hiago <souza8hiago@gmail.com>
2025-12-15 19:50:30 -0300

    Payment, one-to-one association

--- src/main/java/com/hiagodev/course/config/TestConfig.java
+++ src/main/java/com/hiagodev/course/config/TestConfig.java
@@ -35,9 +35,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User("maria","maria@gmail.com","1191919111","122");
         User u2 = new User("mario","mario@gmail.com","11999111","133");
 
-        Order o1 = new Order(null, Instant.parse("2019-07-20T19:54:07Z"), OrderStatus.WAITING_PAYMENT, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-20T19:58:07Z"), OrderStatus.DELIVERED, u1);
-        Order o3 = new Order(null, Instant.parse("2019-07-21T19:57:07Z"), OrderStatus.CANCELED, u2);
+        Order o1 = new Order(null, Instant.parse("2019-07-20T19:54:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-20T19:58:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o3 = new Order(null, Instant.parse("2019-07-21T19:57:07Z"), OrderStatus.WAITING_PAYMENT, u2);
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
@@ -70,5 +70,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-07-20T21:54:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 }
